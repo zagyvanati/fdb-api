20ea479d732a94e0fcb8115987d5520d2e497e12
zagyvanati <zagyvanati@gmail.com>
2022-08-05 14:43:35 +0200

    static root added

--- pages/settings.py
+++ pages/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--p)xk$act_lg@7xst$i9m%%_blwj4&x8#6#p(343_1_ninan8f'
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     'fdbapi.pythonanywhere.com',
+    '127.0.0.1'
 ]
 
 
@@ -119,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = '/home/fdbapi/fdb-api/collected_static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
